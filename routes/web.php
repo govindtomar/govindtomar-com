669072890abbb1a2e499ed253fd34db8a4215ea1
Govind Tomar <govindtomar01@gmail.com>
2022-07-24 17:50:02 +0530

    check

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
 	Route::post('package/publish', [PackageController::class, 'publish'])->name('package.publish');
 });
 
+
+
  // Blog Controller Routes 
 use App\Http\Controllers\Admin\BlogController;
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
